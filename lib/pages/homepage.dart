b4c4d7ddfecc9b3157373314a156b7fb36be7c77
vientrevor <vientrevor@gmail.com>
2022-01-27 02:39:21 +0300

    Created tiles and added fav icon

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: prefer_const_constructors
 
+// import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
+import 'package:wordpair/widgets/random_words.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -8,9 +10,9 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("WordPair Generator"),
-      ),
-    );
+        appBar: AppBar(
+          title: Text("WordPair Generator"),
+        ),
+        body: RandomWords());
   }
 }
---
+++ lib/widgets/random_words.dart
@@ -0,0 +1,41 @@
+// ignore_for_file: non_constant_identifier_names, prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+class RandomWords extends StatefulWidget {
+  const RandomWords({Key? key}) : super(key: key);
+
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final _randomWordPairs = <WordPair>[];
+    final _savedWordPairs = Set<WordPair>();
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (BuildContext context, item) {
+        if (item.isOdd) return Divider();
+        final index = item ~/ 2;
+
+        if (index >= _randomWordPairs.length) {
+          _randomWordPairs.addAll(generateWordPairs().take(10));
+        }
+        final pair = _randomWordPairs[index];
+        final alredySaved = _savedWordPairs.contains(pair);
+        return ListTile(
+            title: Text(
+              pair.asPascalCase,
+              style: TextStyle(fontSize: 18.0),
+            ),
+            trailing: Icon(
+              alredySaved ? Icons.favorite : Icons.favorite_border,
+              color: alredySaved ? Colors.red : null,
+            ));
+      },
+    );
+  }
+}
