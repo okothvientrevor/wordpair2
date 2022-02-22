5547d25a41549763c9ea7d42c29f2675441b9c6d
vientrevor <vientrevor@gmail.com>
2022-02-22 15:19:54 +0300

    update with error

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -2,18 +2,46 @@
 
 // import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
+import 'package:wordpair/pages/fav_words.dart';
 import 'package:wordpair/widgets/random_words.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<String> favs = [];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text("WordPair Generator"),
-          actions: [IconButton(onPressed: () {}, icon: Icon(Icons.list))],
+          title: Text("Word FPair Generator"),
+          actions: [
+            IconButton(
+                onPressed: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => FavWords(
+                            favourites: favs,
+                          )));
+                },
+                icon: Icon(Icons.list))
+          ],
         ),
-        body: RandomWords());
+        body: RandomWords(
+          addToFavs: (String favWord) {
+            setState(() {
+              favs.add(favWord);
+            });
+          },
+          removeFromFavs: (String favWord) {
+            setState(() {
+              favs.remove(favWord);
+            });
+          },
+        ));
   }
 }
